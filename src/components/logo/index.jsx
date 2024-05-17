c39eb4fad388bac59ff95e2007e9b04fbc8c915b
NanoNerd99 <mh.karimi350@gmail.com>
2024-05-17 04:24:44 -0400

    initial commit

---
+++ src/components/logo/index.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+// material-ui
+import { ButtonBase } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import Chip from '@mui/material/Chip';
+
+// project import
+import config from 'config';
+import logo from '../../assets/images/logo.png'
+
+// ==============================|| MAIN LOGO ||============================== //
+
+const LogoSection = ({ sx, to }) => {
+  return (
+    <ButtonBase disableRipple component={Link} to={!to ? config.defaultPath : to} sx={sx}>
+      <Stack direction="row" spacing={1} alignItems="center">
+          <img src={logo} width={200} style={{margin: '15px'}}/>
+      </Stack>
+    </ButtonBase>
+  );
+};
+
+LogoSection.propTypes = {
+  sx: PropTypes.object,
+  to: PropTypes.string
+};
+
+export default LogoSection;
